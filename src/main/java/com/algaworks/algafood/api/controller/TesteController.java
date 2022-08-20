644673ff4459972601ec2f0b54599029f7045424
Marcio Antonio dos Santos <marcioasan@gmail.com>
2022-08-19 21:04:49 -0300

    5.7. Usando as keywords para definir critérios de query methods

--- src/main/java/com/algaworks/algafood/api/controller/TesteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/TesteController.java
@@ -1,6 +1,8 @@
 package com.algaworks.algafood.api.controller;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import com.algaworks.algafood.domain.repository.RestauranteRepository;
 
 //5.1. Implementando consultas JPQL em repositórios
 
@@ -20,8 +24,27 @@ public class TesteController {
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
 	
-//	@GetMapping("/cozinhas/por-nome")
-//	public List<Cozinha> cozinhasPorNome(@RequestParam("nome") String nome) {
-//		return cozinhaRepository.consultarPorNome(nome);
-//	}
+	@Autowired
+	private RestauranteRepository restauranteRepository; 
+	
+	@GetMapping("/cozinhas/por-nome")
+	public List<Cozinha> cozinhasPorNome(@RequestParam("nome") String nome) {
+		return cozinhaRepository.findTodasByNomeContaining(nome);
+	}
+	
+	@GetMapping("/cozinhas/unica-por-nome")
+	public Optional<Cozinha> cozinhaPorNome(String nome) {
+		return cozinhaRepository.findByNome(nome);
+	}
+	
+	@GetMapping("/restaurantes/por-taxa-frete")
+	public List<Restaurante> restaurantesPorTaxaFrete(BigDecimal taxaInicial, BigDecimal taxaFinal) {
+		return restauranteRepository.findByTaxaFreteBetween(taxaInicial, taxaFinal);
+	}
+	
+	@GetMapping("/restaurantes/por-nome")
+	public List<Restaurante> restaurantesPorTaxaFrete(
+			String nome, Long cozinhaId) {
+		return restauranteRepository.findByNomeContainingAndCozinhaId(nome, cozinhaId);
+	}
 }
