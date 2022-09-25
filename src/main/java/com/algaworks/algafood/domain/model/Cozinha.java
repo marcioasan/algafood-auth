5d12c9ce647e42c66cd9e5e160d6b01a3bcb8f02
Marcio Antonio dos Santos <marcioasan@gmail.com>
2022-09-25 13:15:06 -0300

    6.1. Mapeando relacionamento bidirecional com @OneToMany

--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -1,10 +1,14 @@
 package com.algaworks.algafood.domain.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -30,4 +34,9 @@ public class Cozinha {
 	//@JsonProperty("titulo")
 	@Column(nullable = false)
 	private String nome;
+	
+	//6.1. Mapeando relacionamento bidirecional com @OneToMany - 5', 10'10"
+	@JsonIgnore
+	@OneToMany(mappedBy = "cozinha")
+	private List<Restaurante> restaurantes = new ArrayList<>();
 }
