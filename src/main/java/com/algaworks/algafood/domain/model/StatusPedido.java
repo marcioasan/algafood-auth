d3b1b151211ae8cf45a724d2087e640f51ae3381
Marcio Antonio dos Santos <marcioasan@gmail.com>
2022-10-16 15:15:41 -0300

    7.12. Desafio: Criando migrações e mapeando as entidades Pedido e ItemPedido

---
+++ src/main/java/com/algaworks/algafood/domain/model/StatusPedido.java
@@ -0,0 +1,11 @@
+package com.algaworks.algafood.domain.model;
+
+//7.12. Desafio: Criando migrações e mapeando as entidades Pedido e ItemPedido
+
+public enum StatusPedido {
+
+    CRIADO,
+    CONFIRMADO,
+    ENTREGUE,
+    CANCELADO
+}
