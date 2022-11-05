340cb772ebcb7c0f80071dfa0d26d2486dc15834
Marcio Antonio dos Santos <marcioasan@gmail.com>
2022-11-05 20:40:01 -0300

    8.16. Customizando o corpo da resposta padrão de ResponseEntityExceptionHandler

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -2,12 +2,15 @@ package com.algaworks.algafood.api.exceptionhandler;
 
 import java.time.LocalDateTime;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
@@ -15,33 +18,60 @@ import com.algaworks.algafood.domain.exception.NegocioException;
 
 //8.13. Tratando exceções globais com @ExceptionHandler e @ControllerAdvice - 1'28"
 @ControllerAdvice //as exceções de todos os controllers serão tratadas aqui
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler { //8.15. Criando um exception handler global com ResponseEntityExceptionHandler
 
     //8.12. Tratando exceções em nível de controlador com @ExceptionHandler
     @ExceptionHandler(EntidadeNaoEncontradaException.class)
-    public ResponseEntity<?> tratarEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e) {
+    public ResponseEntity<?> tratarEntidadeNaoEncontradaException(EntidadeNaoEncontradaException ex, WebRequest request) {
+    	
+    	//8.16. Customizando o corpo da resposta padrão de ResponseEntityExceptionHandler - 5'
+    	return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
     	
     	//8.12. Tratando exceções em nível de controlador com @ExceptionHandler - 9'
+    	/*
     	Problema problema = Problema.builder()
     			.dataHora(LocalDateTime.now())
     			.mensagem(e.getMessage()).build();
     	
-    	return ResponseEntity.status(HttpStatus.NOT_FOUND)
-    			.body(problema);
+    	return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problema);
+    	*/
+    }
+    
+  //8.14. Desafio: implementando exception handler
+    @ExceptionHandler(EntidadeEmUsoException.class)
+    public ResponseEntity<?> tratarEntidadeEmUsoException(EntidadeEmUsoException ex, WebRequest request) {
+    	
+    	//8.16. Customizando o corpo da resposta padrão de ResponseEntityExceptionHandler - 5'
+    	return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.CONFLICT, request);
     }
     
     @ExceptionHandler(NegocioException.class)
-    public ResponseEntity<?> tratarNegocioException(NegocioException e) {
-    	
-    	//8.12. Tratando exceções em nível de controlador com @ExceptionHandler - 9'
-    	Problema problema = Problema.builder()
-    			.dataHora(LocalDateTime.now())
-    			.mensagem(e.getMessage()).build();
-    	
-    	return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-    			.body(problema);
+    public ResponseEntity<?> tratarNegocioException(NegocioException ex, WebRequest request) {
+
+    	//8.16. Customizando o corpo da resposta padrão de ResponseEntityExceptionHandler - 5'
+    	return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
     
+    //8.16. Customizando o corpo da resposta padrão de ResponseEntityExceptionHandler
+    @Override
+	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
+			HttpStatus status, WebRequest request) {
+		if (body == null) { //8.16. Customizando o corpo da resposta padrão de ResponseEntityExceptionHandler - 9'
+			body = Problema.builder()
+					.dataHora(LocalDateTime.now())
+					.mensagem(status.getReasonPhrase())
+					.build();			
+		} else if(body instanceof String) {
+			body = Problema.builder()
+					.dataHora(LocalDateTime.now())
+					.mensagem((String) body)
+					.build();
+		}
+    	
+		return super.handleExceptionInternal(ex, body, headers, status, request);
+	}
+    
+    /*após estender ResponseEntityExceptionHandler, não precisa dessa implementação - //8.15. Criando um exception handler global com ResponseEntityExceptionHandler
     //Se comentar o método listarXml() de RestauranteController, ao executar o request listar no Postman será lançado HttpMediaTypeNotAcceptableException que será capturado aqui  
     @ExceptionHandler(HttpMediaTypeNotAcceptableException.class)
     public ResponseEntity<?> tratarHttpMediaTypeNotAcceptableException() {
@@ -54,7 +84,9 @@ public class ApiExceptionHandler {
     	return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE)
     			.body(problema);
     }
+    */
     
+    /* após estender ResponseEntityExceptionHandler, não precisa dessa implementação - //8.15. Criando um exception handler global com ResponseEntityExceptionHandler
     @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
     public ResponseEntity<?> tratarHttpMediaTypeNotSupportedException() {
     	
@@ -66,17 +98,5 @@ public class ApiExceptionHandler {
     	return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
     			.body(problema);
     }
-    
-    //8.14. Desafio: implementando exception handler
-    @ExceptionHandler(EntidadeEmUsoException.class)
-    public ResponseEntity<?> tratarEntidadeEmUsoException(EntidadeEmUsoException e) {
-    	
-    	//8.12. Tratando exceções em nível de controlador com @ExceptionHandler - 9'
-    	Problema problema = Problema.builder()
-    			.dataHora(LocalDateTime.now())
-    			.mensagem("Cidade de código 1 não pode ser removida, pois está em uso").build();
-    	
-    	return ResponseEntity.status(HttpStatus.CONFLICT)
-    			.body(problema);
-    }
+    */
 }
