ab56f968f8fedf3f3275fa4b70c7424da05a5e59
Marcio Antonio dos Santos <marcioasan@gmail.com>
2022-11-29 15:07:47 -0300

    9.15. Criando constraints de validação customizadas usando composição

--- src/main/java/com/algaworks/algafood/core/validation/ValidationConfig.java
+++ src/main/java/com/algaworks/algafood/core/validation/ValidationConfig.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.core.validation;
 
 import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
@@ -9,6 +10,7 @@ import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 @Configuration
 public class ValidationConfig {
 
+	@Bean
 	public LocalValidatorFactoryBean validator(MessageSource messageSource) {
 		LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
 		bean.setValidationMessageSource(messageSource); //configura para usar o messages.properties, se tirar essa linha, vai usar o ValidationMessages.properties
