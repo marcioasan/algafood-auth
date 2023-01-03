cfa5e03cecc54aaebb951dff86155319d57b7e6a
Marcio Antonio dos Santos <marcioasan@gmail.com>
2023-01-02 21:38:01 -0300

    12.5. Desafio: implementando os endpoints de formas de pagamento

---
+++ src/main/java/com/algaworks/algafood/api/model/input/FormaPagamentoInput.java
@@ -0,0 +1,16 @@
+package com.algaworks.algafood.api.model.input;
+
+import javax.validation.constraints.NotBlank;
+
+import lombok.Getter;
+import lombok.Setter;
+
+//12.5. Desafio: implementando os endpoints de formas de pagamento
+
+@Setter
+@Getter
+public class FormaPagamentoInput {
+
+    @NotBlank
+    private String descricao;
+}
