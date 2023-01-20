4972b8bb7cabe28d5c6dc42c492c77ce74042235
Marcio Antonio dos Santos <marcioasan@gmail.com>
2023-01-19 21:36:03 -0300

    12.22. Implementando endpoint de transição de status de pedidos

--- src/main/java/com/algaworks/algafood/domain/model/StatusPedido.java
+++ src/main/java/com/algaworks/algafood/domain/model/StatusPedido.java
@@ -1,11 +1,22 @@
 package com.algaworks.algafood.domain.model;
 
 //7.12. Desafio: Criando migrações e mapeando as entidades Pedido e ItemPedido
+//12.22. Implementando endpoint de transição de status de pedidos - 16'50" - foi colocada a descrição dos status
 
 public enum StatusPedido {
 
-    CRIADO,
-    CONFIRMADO,
-    ENTREGUE,
-    CANCELADO
+	CRIADO("Criado"),
+	CONFIRMADO("Confirmado"),
+	ENTREGUE("Entregue"),
+	CANCELADO("Cancelado");
+	
+	private String descricao;
+	
+	StatusPedido(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public String getDescricao() {
+		return this.descricao;
+	}
 }
---
+++ src/main/java/com/algaworks/algafood/domain/service/FluxoPedidoService.java
@@ -0,0 +1,35 @@
+package com.algaworks.algafood.domain.service;
+
+import java.time.OffsetDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.algaworks.algafood.domain.exception.NegocioException;
+import com.algaworks.algafood.domain.model.Pedido;
+import com.algaworks.algafood.domain.model.StatusPedido;
+
+//12.22. Implementando endpoint de transição de status de pedidos - 8'30", 17'20"
+@Service
+public class FluxoPedidoService {
+
+	@Autowired
+	private EmissaoPedidoService emissaoPedido;
+	
+	@Transactional
+	public void confirmar(Long pedidoId) {
+		Pedido pedido = emissaoPedido.buscarOuFalhar(pedidoId);
+		
+		if (!pedido.getStatus().equals(StatusPedido.CRIADO)) {
+			throw new NegocioException(
+					String.format("Status do pedido %d não pode ser alterado de %s para %s",
+							pedido.getId(), pedido.getStatus().getDescricao(), 
+							StatusPedido.CONFIRMADO.getDescricao()));
+		}
+		
+		pedido.setStatus(StatusPedido.CONFIRMADO);
+		pedido.setDataConfirmacao(OffsetDateTime.now());
+	}
+	
+}
