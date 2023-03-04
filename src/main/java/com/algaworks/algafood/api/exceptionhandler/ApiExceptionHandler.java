fcc9b24a52de4e3658aea721c93148d863f5c447
Marcio Antonio dos Santos <marcioasan@gmail.com>
2023-03-03 21:35:31 -0300

    14.16. Corrigindo exception handler de media type não aceita

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -16,6 +16,7 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -63,6 +64,12 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	    		.build();
 
 	    return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
+	}
+	
+	//14.16. Corrigindo exception handler de media type não aceita - 6'
+	protected ResponseEntity<Object> handleHttpMediaTypeNotAcceptable(HttpMediaTypeNotAcceptableException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		return ResponseEntity.status(status).headers(headers).build();
 	}
 	
 	//13.7. Tratando BindException ao enviar parâmetros de URL inválidos - 2'
