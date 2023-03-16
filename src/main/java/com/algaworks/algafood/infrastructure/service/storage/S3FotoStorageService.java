2552b84930945cb39070e165b7483e55654e1fdd
Marcio Antonio dos Santos <marcioasan@gmail.com>
2023-03-15 21:09:20 -0300

    14.24. Desafio: Implementando a exclusão de objetos do bucket da Amazon S3

--- src/main/java/com/algaworks/algafood/infrastructure/service/storage/S3FotoStorageService.java
+++ src/main/java/com/algaworks/algafood/infrastructure/service/storage/S3FotoStorageService.java
@@ -9,6 +9,7 @@ import com.algaworks.algafood.core.storage.StorageProperties;
 import com.algaworks.algafood.domain.service.FotoStorageService;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 
@@ -55,8 +56,18 @@ public class S3FotoStorageService implements FotoStorageService {
 		return String.format("%s/%s", storageProperties.getS3().getDiretorioFotos(), nomeArquivo);
 	}
 
+	//14.24. Desafio: Implementando a exclusão de objetos do bucket da Amazon S3
 	@Override
 	public void remover(String nomeArquivo) {
-	}
+		try {
+			String caminhoArquivo = getCaminhoArquivo(nomeArquivo);
 
+			var deleteObjectRequest = new DeleteObjectRequest(
+					storageProperties.getS3().getBucket(), caminhoArquivo);
+
+			amazonS3.deleteObject(deleteObjectRequest);
+		} catch (Exception e) {
+			throw new StorageException("Não foi possível excluir arquivo na Amazon S3.", e);
+		}
+	}
 }
