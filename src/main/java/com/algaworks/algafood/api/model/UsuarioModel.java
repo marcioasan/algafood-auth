1bdda1f812470a6b8b574f4d9688833282081c94
Marcio Antonio dos Santos <marcioasan@gmail.com>
2023-05-03 20:47:07 -0300

    19.12. Desafio: adicionando hypermedia nos recursos de usuários

--- src/main/java/com/algaworks/algafood/api/model/UsuarioModel.java
+++ src/main/java/com/algaworks/algafood/api/model/UsuarioModel.java
@@ -1,11 +1,15 @@
 package com.algaworks.algafood.api.model;
 
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.core.Relation;
+
 import lombok.Getter;
 import lombok.Setter;
 
+@Relation(collectionRelation = "usuarios")
 @Setter
 @Getter
-public class UsuarioModel {
+public class UsuarioModel extends RepresentationModel<UsuarioModel>{
 
     private Long id;
     private String nome;
