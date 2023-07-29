476ec0b4124fed4a1d89cc7513f1bfb446af5db5
Marcio Antonio dos Santos <marcioasan@gmail.com>
2023-07-29 16:16:37 -0300

    19.30. Desafio: adicionando links de associação de restaurantes com responsáveis

--- src/main/java/com/algaworks/algafood/api/AlgaLinks.java
+++ src/main/java/com/algaworks/algafood/api/AlgaLinks.java
@@ -253,4 +253,18 @@ public class AlgaLinks {
 		return linkTo(methodOn(RestauranteFormaPagamentoController.class)
 				.associar(restauranteId, null)).withRel(rel);
 	}
+	
+	//19.30. Desafio: adicionando links de associação de restaurantes com responsáveis - INÍCIO
+	public Link linkToRestauranteResponsavelDesassociacao(
+			Long restauranteId, Long usuarioId, String rel) {
+
+		    return linkTo(methodOn(RestauranteUsuarioResponsavelController.class)
+		            .desassociar(restauranteId, usuarioId)).withRel(rel);
+		}
+
+		public Link linkToRestauranteResponsavelAssociacao(Long restauranteId, String rel) {
+		    return linkTo(methodOn(RestauranteUsuarioResponsavelController.class)
+		            .associar(restauranteId, null)).withRel(rel);
+		}
+	//19.30. Desafio: adicionando links de associação de restaurantes com responsáveis - FIM
 }
--- src/main/java/com/algaworks/algafood/api/controller/RestauranteUsuarioResponsavelController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteUsuarioResponsavelController.java
@@ -1,11 +1,10 @@
 package com.algaworks.algafood.api.controller;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -35,6 +34,43 @@ public class RestauranteUsuarioResponsavelController {
     @Autowired
     private AlgaLinks algaLinks;  //19.21. Desafio: refatorando construção e inclusão de links
     
+    
+	
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public CollectionModel<UsuarioModel> listar(@PathVariable Long restauranteId) {
+		Restaurante restaurante = cadastroRestaurante.buscarOuFalhar(restauranteId);
+
+		CollectionModel<UsuarioModel> usuariosModel = usuarioModelAssembler
+				.toCollectionModel(restaurante.getResponsaveis())
+				.removeLinks()
+				.add(algaLinks.linkToResponsaveisRestaurante(restauranteId))
+				.add(algaLinks.linkToRestauranteResponsavelAssociacao(restauranteId, "associar"));
+
+		usuariosModel.getContent().stream().forEach(usuarioModel -> {
+			usuarioModel.add(algaLinks.linkToRestauranteResponsavelDesassociacao(
+					restauranteId, usuarioModel.getId(), "desassociar"));
+		});
+
+		return usuariosModel;
+	}
+    
+	@DeleteMapping("/{usuarioId}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public ResponseEntity<Void> desassociar(@PathVariable Long restauranteId, @PathVariable Long usuarioId) {
+		cadastroRestaurante.desassociarResponsavel(restauranteId, usuarioId);
+
+		return ResponseEntity.noContent().build();
+	}
+    
+	@PutMapping("/{usuarioId}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public ResponseEntity<Void> associar(@PathVariable Long restauranteId, @PathVariable Long usuarioId) {
+		cadastroRestaurante.associarResponsavel(restauranteId, usuarioId);
+
+		return ResponseEntity.noContent().build();
+	}
+	
+    /*
     @GetMapping
     public CollectionModel<UsuarioModel> listar(@PathVariable Long restauranteId) {
         Restaurante restaurante = cadastroRestaurante.buscarOuFalhar(restauranteId);
@@ -43,14 +79,14 @@ public class RestauranteUsuarioResponsavelController {
                 .removeLinks()
                 .add(algaLinks.linkToResponsaveisRestaurante(restauranteId));
     	
-    	/*
-        Restaurante restaurante = cadastroRestaurante.buscarOuFalhar(restauranteId);
-        
-        return usuarioModelAssembler.toCollectionModel(restaurante.getResponsaveis())
-        		.removeLinks()
-        		.add(linkTo(methodOn(RestauranteUsuarioResponsavelController.class)
-        				.listar(restauranteId)).withSelfRel()); //19.13. Corrigindo link de coleção de recurso de responsáveis por restaurante - 2'
-        */
+    	
+//        Restaurante restaurante = cadastroRestaurante.buscarOuFalhar(restauranteId);
+//        
+//        return usuarioModelAssembler.toCollectionModel(restaurante.getResponsaveis())
+//        		.removeLinks()
+//        		.add(linkTo(methodOn(RestauranteUsuarioResponsavelController.class)
+//        				.listar(restauranteId)).withSelfRel()); //19.13. Corrigindo link de coleção de recurso de responsáveis por restaurante - 2'
+//        
     }
     
     @DeleteMapping("/{usuarioId}")
@@ -64,4 +100,5 @@ public class RestauranteUsuarioResponsavelController {
     public void associar(@PathVariable Long restauranteId, @PathVariable Long usuarioId) {
         cadastroRestaurante.associarResponsavel(restauranteId, usuarioId);
     }
+    */
 }
